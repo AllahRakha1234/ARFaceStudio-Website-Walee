b89b10594e3bef9a4c349ec564a3a47759290fbd
AllahRakha1234 <allahrakhahsp1234@gmail.com>
2024-09-14 01:13:46 +0500

    init

---
+++ face-studio/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import './globals.css';
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center mt-[40vh]">
+      <h2 className="text-3xl font-bold underline bg-red-400 p-3 rounded-md">Welcome to AR Face Studio</h2>
+    </div>
+  )
+}
