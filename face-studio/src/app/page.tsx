1d62e40c01d00bd9553fae5139f8ba28fc2bb9fc
AllahRakha1234 <allahrakhahsp1234@gmail.com>
2024-09-14 12:31:05 +0500

    dir setup

--- face-studio/src/app/page.tsx
+++ face-studio/src/app/page.tsx
@@ -1,10 +1,14 @@
 
 import './globals.css';
+import Navbar from './../Components/Navbar';
 
 export default function Home() {
   return (
+    <>
+    <Navbar />
     <div className="flex justify-center items-center mt-[40vh]">
       <h2 className="text-3xl font-bold underline bg-red-400 p-3 rounded-md">Welcome to AR Face Studio</h2>
     </div>
+    </>
   )
 }
