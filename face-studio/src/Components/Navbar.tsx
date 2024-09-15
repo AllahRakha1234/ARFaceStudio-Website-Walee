30cd501147287ccdc2be907d8094becf1a893426
AllahRakha1234 <allahrakhahsp1234@gmail.com>
2024-09-15 14:20:35 +0500

    Navbar modify

--- face-studio/src/Components/Navbar.tsx
+++ face-studio/src/Components/Navbar.tsx
@@ -27,22 +27,6 @@ const Navbar = () => {
     <div ref={navbarRef} className="border-1 p-3 bg-white text-black flex flex-row items-center justify-between">
       {/* Logo */}
       <img src="/walee-logo.png" className="w-[10%] h-[10%]" alt="Logo" />
-
-      {/* Navigation links */}
-      <ul className="flex text-lg items-center mx-4 flex-row gap-8">
-        <li className="relative cursor-pointer after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-black hover:after:w-full after:transition-all after:duration-300">
-          <Link href="/AssetsPage">My Assets</Link>
-        </li>
-        <li className="relative cursor-pointer after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-black hover:after:w-full after:transition-all after:duration-300">
-          <Link href="/about">About us</Link>
-        </li>
-        <li className="relative cursor-pointer after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-black hover:after:w-full after:transition-all after:duration-300">
-          <Link href="/services">Services</Link>
-        </li>
-        <li className="relative cursor-pointer after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-black hover:after:w-full after:transition-all after:duration-300">
-          <Link href="/contact">Contact us</Link>
-        </li>
-      </ul>
     </div>
   );
 };
